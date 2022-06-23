6be39b3b852054ff3dfce53e4124e56da2558689
Marcelo de Freitas Moraes <mamoressp@gmail.com>
2022-06-23 08:18:26 -0300

    api home

--- components/Main/index.js
+++ components/Main/index.js
@@ -6,6 +6,7 @@ export const Main = () => {
     return (
         
             <S.Main>
+
                 <S.SubMain>
                 <S.ImgMain src='banner2.jpg' alt="Vercel Logo"/> 
                 </S.SubMain>
@@ -21,11 +22,12 @@ export const Main = () => {
                 <S.SubMain>
                 <S.ImgMain src='banner4.gif' alt="Vercel Logo"/> 
                 </S.SubMain>
-                
+
                 <div>
                     <S.CredCards>Aceitamos Todas esses Formas de Pagamentos e parcelamos tudo em até 10x sem juros</S.CredCards>
                     <S.ImgCards  src='cardtodos.png' alt="Vercel Logo"/>
                 </div>
+                
             </S.Main>
         
     )
